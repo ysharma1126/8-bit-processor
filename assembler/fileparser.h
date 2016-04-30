75996b271df4dac6b52748765f8092fb19de3fa9
Shalin <shalinpatel75@gmail.com>
2016-04-30 16:56:31 -0400

    Added Data Class to fileparser header

--- assembler/fileparser.h
+++ assembler/fileparser.h
@@ -4,7 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
+class Data{
+public:
+	std::string name; //holds name of data
+	std::string value;//holds value of data
+	std::string address;//holds address of data
+	Data(std::string n, std::string v, std::string a);
+};
+void storeData(std::string assembly_name);
 bool checkFileExists(std::string file_name);
 void labelToBinary(std::string input_file, std::string output_file);
 void assemblyToBinary(std::string input_file, std::string output_file);
